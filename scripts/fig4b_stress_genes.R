e67b7270136523e0ce245941db6860b6edb6e2ee
sofia-urosa <hedgehog.dilemma@outlook.com>
2025-12-11 17:55:30 +0000

    Commented further, fixed graph colors and legend, changed cutoff for TP E for 10kb

--- scripts/fig4b_stress_genes.R
+++ scripts/fig4b_stress_genes.R
@@ -8,6 +8,10 @@
 #   2. Where are they? 
 #   3. Are they related to a stress pathway?
 
+# when fusing all of chromosomes together, one of the mechanisms affected
+# is related to TPE (Telomere proximity effect) - we want to know how many
+# of the sig. expressed genes got up or down regulated
+
 # get output from DESeq for expression
 res <- read.csv("outputs/rnaseq/expression_analysis/full_dseq_res.csv", header = TRUE, check.names = FALSE)
 
@@ -52,7 +56,7 @@ gene_table <- data.frame(
 # fasta index for distance to telomeres
 # we only need chr and len (col1 col2)
 
-# fai -> chromosome size
+# fai -> chromosome size (lenght)
 fai <- read.table(
   file.path("originals/ref/S288C.fa.fai"),
   header = FALSE,
@@ -62,27 +66,36 @@ fai <- read.table(
 colnames(fai) <- c("chr", "length", "offset", "linelen", "linebytes")
 chr_len <- setNames(fai$length, fai$chr)
 
+
 gene_table$chr_len <- chr_len[gene_table$chr]
+
+# calculate 'base' position of gene
 gene_table$mid <- (gene_table$start + gene_table$end)/2
 
+# distance to 'nearest' telomere
 gene_table$dist_to_telo <- pmin(gene_table$mid -1, gene_table$chr_len - gene_table$mid)
 
 summary(gene_table$dist_to_telo)
 
+# merge by gene id so we have chr, start, end, distance to telomere
 res_annot <- merge(res, gene_table[, c("gene_id", "chr", "start", "end", "dist_to_telo")], by = "gene_id", all.x = TRUE)
 
+#filter by significant genes according to paper
 sig <- subset(res_annot,
               abs(log2FoldChange) >= 1 & padj < 0.001)
 
 head(res_annot)
 
-cutoff <- 20000
+# define cutoff - how many bases is 'close' to a telomere? 
+cutoff <- 10000
 
 sig$tpe_flag <- !is.na(sig$dist_to_telo) & sig$dist_to_telo <= cutoff
 
 table(sig$tpe_flag)
   
-###
+### authors do not define how they got the stress genes
+# so we use the GO ontology
+# we need: go_id, orf
 obo_file <- file.path("originals/ref/go.obo")
 obo <- readLines(obo_file)
 
@@ -99,16 +112,18 @@ GO_names <- c()
 
 for (i in seq_along(term_starts)) {
   block <- get_block(i)
-  
+  # find lines that have id: GO: and name:
   id_line <- block[grep("^id: GO:", block)]
   name_line <- block[grep("^name:", block)]
   
   # Skip malformed terms
   if (length(id_line) != 1 || length(name_line) != 1) next
   
+  #remove id and name pfefixes
   go_id <- sub("^id: ", "", id_line)
   go_name <- sub("^name: ", "", name_line)
   
+  #build a go mapping table
   GO_IDs   <- c(GO_IDs, go_id)
   GO_names <- c(GO_names, go_name)
 }
@@ -119,6 +134,7 @@ go_map <- data.frame(
   stringsAsFactors = FALSE
 )
 
+# we use the gaf file to connect to the go file using go_id. 
 gaf_file <- file.path("originals/ref/gene_association.sgd.gaf")
 
 gaf <- read.table(
@@ -131,16 +147,19 @@ gaf <- read.table(
 )
 
 # ORF name from synonyms column
+# "YGR149W|glycerophosphocholine acyltransferase" -> split by |
 extract_orf <- function(x) {
   if (is.na(x) || x == "") return(NA)
   strsplit(x, "\\|")[[1]][1]
 }
 
+# we'll attempt to find all genes related to stress
 gaf$ORF <- vapply(gaf$V11, extract_orf, character(1))
 
 colnames(gaf)[5] <- "GO_ID"
 
 # Merge
+# orf -> go_id -> go_name
 gaf2 <- merge(
   gaf[, c("ORF", "GO_ID")],
   go_map,
@@ -148,15 +167,23 @@ gaf2 <- merge(
   all.x = TRUE
 )
 
+# define "stress-related gene" as any ORF whose GO_name contains "stress"
+# (best attempt- paper authors didnt specify how they did it)
 stress_rows <- grep("stress", gaf2$GO_name, ignore.case = TRUE)
 stress_genes <- unique(gaf2$ORF[stress_rows])
+
+# stress_flag = true
 sig$stress_flag <- sig$gene_id %in% stress_genes
 
+# assign class (TPE, stress or other)
 sig$class <- ifelse(sig$tpe_flag, "TPE",
                     ifelse(sig$stress_flag, "Stress", "Other"))
 
 table(sig$class)
 
+# regulation
+#   - up   = log2FoldChange > 0
+#   - down = log2FoldChange < 0
 count_ud <- function(df, cls) {
   d <- df[df$class == cls, ]
   up   <- sum(d$log2FoldChange > 0)
@@ -172,24 +199,36 @@ df_counts <- rbind(
 
 df_counts
 
-# build matrix for barplot
+# build matrix for barplot (col = class, row=up down)
 mat <- t(as.matrix(df_counts[, c("up", "down")]))
 colnames(mat) <- df_counts$class
 rownames(mat) <- c("Upregulated", "Downregulated")
 
-# optional: save to file
-png("/scratch/users/k25119154/cc/single-chromosome-yeast-repro/outputs/rnaseq/fig4b_reproduced.png",
+cols <- c("red3", "green4")
+
+# save to file
+png("outputs/rnaseq/fig4b_reproduced.png",
     width = 800, height = 600)
 
 barplot(
   mat,
   beside = FALSE,               # stacked
-  col = c("red", "green"),
+  col = cols,
   ylab = "Number of genes",
   main = "Classification of DE genes (SY14 vs BY4742)",
-  legend.text = TRUE,
-  args.legend = list(x = "topright", bty = "n")
+  ylim = c(0, max(colSums(mat)) * 1.25)
+  
 )
 
+legend(
+  "bottom",
+  inset = -0.15,
+  horiz = TRUE,
+  legend = c("Upregulated", "Downregulated"),
+  fill = cols,
+  bty = "n",
+  cex = 1.2,
+  xpd = TRUE
+)
 dev.off()
 
